382fd5f8b394cdd7e65d24797b300d22405812f1
JodyZ0203 <Jody625326@gmail.com>
2020-12-19 16:29:30 -0500

    commit on main

--- main.py
+++ main.py
@@ -3,18 +3,4 @@ Using machine learning to aid the diagnosis of COVID-19 in patients.
 Author: Batuhan Aktan, Jody Zhou, Sam Zhang
 Date: DEC 2020
 '''
-<<<<<<< HEAD
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras.preprocessing import image
-import cv2
-
-file ='./DataSets/HealthyLungs/Healthy.jpg'
-img = cv2.imread(file)
-cv2.imshow('Image', img)
-cv2.waitkey(0)
-
-input()
-=======
 import numpy
->>>>>>> 69954be10afbb192f5a5d038df51c814205431b6
