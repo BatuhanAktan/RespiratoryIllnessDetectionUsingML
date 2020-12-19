ebaba7c867b420695977429e773ec9d5c909fd40
BatuhanAktan <43014701+BatuhanAktan@users.noreply.github.com>
2020-12-19 16:03:32 -0500

    Image Prep

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ Using machine learning to aid the diagnosis of COVID-19 in patients.
 Author: Batuhan Aktan, Jody Zhou, Sam  ...
 Date: DEC 2020
 '''
-import numpy
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import cv2
+
+file ='./DataSets/HealthyLungs/Healthy.jpg'
+img = cv2.imread(file)
+cv2.imshow('Image', img)
+cv2.waitkey(0)
+
+input()
