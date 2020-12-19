69954be10afbb192f5a5d038df51c814205431b6
Jody Zhou <56443135+JodyZ0203@users.noreply.github.com>
2020-12-19 15:17:51 -0500

    name change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 Using machine learning to aid the diagnosis of COVID-19 in patients.
-Author: Batuhan Aktan, Jody Zhou, Sam  ...
+Author: Batuhan Aktan, Jody Zhou, Sam Zhang
 Date: DEC 2020
 '''
 import numpy
