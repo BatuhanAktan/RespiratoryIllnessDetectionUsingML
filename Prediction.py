e7ba3522c71747b9fa902e8c3d16fb89653b7caa
BatuhanAktan <43014701+BatuhanAktan@users.noreply.github.com>
2020-12-20 08:09:23 -0500

    Final Changes

--- Prediction.py
+++ Prediction.py
@@ -11,12 +11,14 @@ categories = ['Healthy', 'Sick']
 
 def prepare(file):
     imgArr = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    newArr = cv2.resize(imgArr, (600,600))
+    imgArr = cv2.resize(imgArr, (600,600))
     
-    return np.array(newArr).reshape(-1, 600,600,1)
+    return np.array(imgArr).reshape(-1, 600,600,1)
 
 
-prediction = model.predict([prepare('IM-0166-0001.jpeg')])
+prediction = model.predict([prepare('./TestImages/IM-0001-0001.jpeg')])
+prediction2 = model.predict([prepare('./TestImages/person15_virus_46.jpeg')])
 
 
-print(categories[int(prediction[0][0])])
+print('Image being processed:', 'IM-0001-0001.jpeg\nExpected Result: Healthy','\nResult:',categories[int(prediction[0][0])])
+print('Image being processed:', 'person15_virus_46.jpeg\nExpected Result: Sick','\nResult:',categories[int(prediction2[0][0])])
