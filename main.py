0a9d0f17ce17274b691d93745b532cb2ffc0b5c4
BatuhanAktan <43014701+BatuhanAktan@users.noreply.github.com>
2020-12-19 19:08:59 -0500

    Fixes to Datasets

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ Using machine learning to aid the diagnosis of COVID-19 in patients.
 Author: Batuhan Aktan, Jody Zhou, Sam  ...
 Date: DEC 2020
 '''
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras.preprocessing import image
-import cv2
 import os
 import random
 import pickle
-
+import cv2
+import numpy as np
 
 data ='./DataSets/chest_xray/train'
 category = ['Healthy', 'Pneumonia']
@@ -25,16 +22,24 @@ def createTrainingData():
         classNum = category.index(element)
         for file in os.listdir(path):
             imgArray = cv2.imread(os.path.join(path, file), cv2.IMREAD_GRAYSCALE)
+            imgArray = cv2.resize(imgArray, (600,600))
             trainingData.append([imgArray, classNum])
     random.shuffle(trainingData)
     return trainingData
 
 
 def MLPrep(data):
+    global X
+    global y
     for arr, cat in data:
         X.append(arr)
         y.append(cat)
 
+        
+    X = np.array(X).reshape(-1,600,600,1)
+    y = np.array(y)
+
+    
     #saves training data X.
     pickleOut = open("X.pickle", "wb")
     pickle.dump(X, pickleOut)
